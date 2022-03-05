936b5b7f46170f8105dcdac9b620071b31a81789
HairZabala <34761166+HairZabala@users.noreply.github.com>
2022-03-05 19:15:10 +1000

    Process ID

--- lib/backend_web/controllers/page_controller.ex
+++ lib/backend_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule BackendWeb.PageController do
   use BackendWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", process_id: self |> :erlang.pid_to_list |> to_string) 
   end
 end
